c516a6fa08b8bef77e4e7690d050117770b972e0
Junki Kizuka <k.falconws@gmail.com>
2014-10-15 21:04:07 +0900

    add rdiff backup command.

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from fabric.api import task, run, get
+from fabric.api import task, run, get, local
 
 @task
 def get_data():
@@ -11,6 +11,9 @@ def get_data():
     dump_file = "redmine.sql"
     attach_file = "/var/lib/redmine/default/files"
     pg_dump = "pg_dump -f %s -U redmine redmine" % dump_file
+    rdiff_dir = "/home/mint/rdiff_backup_dir"
     run(pg_dump)
     get(dump_file)
     get(attach_file)
+    local("rdiff-backup /home/mint/backup %s" % rdiff_dir)
+    local("rdiff-backup --remove-older-than 2W %s" % rdiff_dir)
